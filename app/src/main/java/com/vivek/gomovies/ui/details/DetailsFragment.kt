56c6c1edc2ec5ea282379bb207dbb81d8d943866
Vivek <>
2024-12-24 09:20:21 +0530

    Fixed Details UI

--- app/src/main/java/com/vivek/gomovies/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/vivek/gomovies/ui/details/DetailsFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import coil.load
+import com.vivek.gomovies.R
 import com.vivek.gomovies.databinding.FragmentDetailsBinding
 import com.vivek.gomovies.ui.details.adaptor.ImagesAdapter
 import com.vivek.gomovies.ui.viewModel.PeopleViewModel
@@ -26,6 +28,9 @@ class DetailsFragment : Fragment() {
     private var personId: Int = -1
     private lateinit var imagesAdapter: ImagesAdapter
 
+    private var isBioExpanded = false
+    private val maxBioLines = 4
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -39,12 +44,17 @@ class DetailsFragment : Fragment() {
         arguments?.let {
             personId = it.getInt("person_id", -1)
         }
+      //  binding.toolbar.setNavigationIcon(R.drawable.arrow_back)
 
         if (personId != -1) {
             viewModel.fetchPersonImages(personId)
             observePersonDetails(personId)
             observePersonImageList()
             setupImagesRecyclerView()
+        }
+
+        binding.readMore.setOnClickListener {
+            toggleBioText()
         }
 
     }
@@ -62,7 +72,12 @@ class DetailsFragment : Fragment() {
             viewModel.getPersonDetails(personId).collect { personDetails ->
                 binding.personName.text = personDetails.name
                 binding.personBio.text = personDetails.biography
-                binding.ivProfile.load(personDetails.real_path)
+                binding.blurredBackground.load(personDetails.real_path)
+                binding.profileImage.load(personDetails.real_path)
+
+                binding.personBio.maxLines = maxBioLines
+                binding.readMore.isVisible = personDetails.biography?.length!! > 200
+
             }
         }
     }
@@ -72,6 +87,16 @@ class DetailsFragment : Fragment() {
         binding.recyclerViewImages.apply {
             layoutManager = GridLayoutManager(requireContext(), 3)
             adapter = imagesAdapter
+        }
+    }
+    private fun toggleBioText() {
+        isBioExpanded = !isBioExpanded
+        if (isBioExpanded) {
+            binding.personBio.maxLines = Int.MAX_VALUE
+            binding.readMore.text = "Read Less"
+        } else {
+            binding.personBio.maxLines = maxBioLines
+            binding.readMore.text = "Read More"
         }
     }
 
